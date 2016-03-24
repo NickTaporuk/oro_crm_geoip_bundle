fa14bb300b31a221c49ca5815eda03b3690da69b
NickTaporuk <nictaporuk@mail.ru>
2016-03-24 20:48:59 +0200

    Update GeoipController.php

--- GeoipBundle/Controller/GeoipController.php
+++ GeoipBundle/Controller/GeoipController.php
@@ -4,7 +4,7 @@ namespace Nabludai\GeoipBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
-use \Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
